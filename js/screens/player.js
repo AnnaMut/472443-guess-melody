e29710a4e2a5b8d184f7189a6817db75c36016e7
AnnaMut <butorovaanna@yandex.ru>
2018-09-09 23:14:43 +0300

    Доработка

--- js/screens/player.js
+++ js/screens/player.js
@@ -2,7 +2,7 @@ export const playerGenre = (src) => `
 <div class="track">
   <button class="track__button track__button--play" type="button"></button>
   <div class="track__status">
-  <audio src="${src}" preload="auto" preload="auto"></audio>
+  <audio src="${src}" preload="auto"></audio>
 </div>`;
 
 export const playerArtist = (src) => `
